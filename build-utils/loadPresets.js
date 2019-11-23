ac99e39dd19dba0cafda8504d9f9cd2ad43971e2
Konstantin6487 <Konstantin6487@rambler.ru>
2019-11-23 20:39:46 +0300

    add lazy-load

---
+++ build-utils/loadPresets.js
@@ -0,0 +1,12 @@
+const webpackMerge = require('webpack-merge');
+
+const applyPresets = (env) => {
+  const { presets = [] } = env;
+  /** @type {string[]} */
+  const mergedPresets = [presets].flat();
+  const mergedConfigs = mergedPresets
+    .map((presetName) => require(`./presets/webpack.${presetName}`)(env));
+  return webpackMerge({}, ...mergedConfigs);
+};
+
+module.exports = applyPresets;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import nav from './nav';
-import { footer } from './footer';
+// import { footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
 import makeImage from './image';
@@ -8,12 +8,21 @@ import imageUrl from './webpack-logo.jpg';
 import './button.css';
 import './footer.css';
 
+// import footer from './footer';
+const loadFooter = () => import('./footer');
+
 const image = makeImage(imageUrl);
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 
+button.addEventListener('click', (e) => {
+  loadFooter().then((m) => {
+    document.body.appendChild(m.footer);
+  })
+});
+
 document.body.appendChild(button);
-document.body.appendChild(footer);
+
 document.body.appendChild(image);
 
 console.log(
