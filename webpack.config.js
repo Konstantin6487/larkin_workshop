9f25fc8e24c9bb3e5d3df08ee1abc8fcc90ff6dc
Konstantin6487 <Konstantin6487@rambler.ru>
2019-11-23 15:00:02 +0300

    fixes, webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpackMerge = require('webpack-merge');
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
-module.exports = ({ mode }) => {
-  return ({
+module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
+  webpackMerge({
     mode,
     output: {
       filename: 'bundle.js',
     },
-    plugins: [
-      new HtmlWebpackPlugin(),
-      new webpack.ProgressPlugin(),
-    ]
-  });
-};
+    plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+  },
+  modeConfig(mode),
+);
 
