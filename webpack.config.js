75d733f9894dbd7704cf040d3e368c74b4c5cce0
Konstantin6487 <Konstantin6487@rambler.ru>
2019-11-23 19:36:11 +0300

    add presets

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
+const presetsConfig = require('./build-utils/loadPresets');
 
 module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
   webpackMerge({
@@ -20,5 +21,6 @@ module.exports = ({ mode, presets } = { mode: 'production', presets: [] }) =>
     plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
   },
   modeConfig(mode),
+  presetsConfig({ mode, presets })
 );
 
